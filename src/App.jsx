95fcfe6a98fed00f5fe937985b40139e4d858693
triabagus <triatop9@gmail.com>
2020-07-27 13:52:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
+class App extends Component { 
+    render() { 
+        return (
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+          );
+    }
+}
+ 
+export default App;
