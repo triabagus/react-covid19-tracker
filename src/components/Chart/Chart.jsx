6d9effec0a50cf26c987ae346c1105f7b5b811f9
triabagus <triatop9@gmail.com>
2020-07-27 23:26:41 +0700

    add image and responsive

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths}, country}) => {
     const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
@@ -12,7 +12,7 @@ const Chart = () => {
         }
 
         fetchAPI();
-    });
+    },[]);
 
     const lineChart = (
         dailyData.length
@@ -36,10 +36,35 @@ const Chart = () => {
             />) : null
     );
 
+    const barChart = (
+        confirmed
+            ? (
+                <Bar
+                    data={{
+                        labels:['Infected', 'Recovered', 'Deaths'],
+                        datasets: [{
+                            label: 'People',
+                            backgroundColor: [
+                                '#00bcd4',
+                                '#4caf50',
+                                '#f44336'
+                            ],
+                            data:[confirmed.value, recovered.value, deaths.value]
+                        }]
+                    }}
+                    
+                    options={{
+                        legend: { display: false },
+                        title: {display:true, text:`Current state in ${country}`},
+                    }}
+                />
+            ):null
+    );
+
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart: lineChart}
         </div>
     )
 }
